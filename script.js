f58eb92d2180074d8d9b5ae0885f1d377acef48f
RomuloBrumDev <romulobrum28@gmail.com>
2022-06-11 00:43:05 -0300

    primeiro desafio de logica

---
+++ script.js
@@ -0,0 +1,28 @@
+// Crie uma variável que receba um nome;
+// Crie uma variável que receba um número;
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+//Mostre no console a mensagem "Estou mexendo no console.log";
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+
+let nome = "Romulo";
+console.log(nome);
+
+let numero = 42;
+console.log(numero);
+
+let calculateYear = 2022;
+console.log(calculateYear - 42);
+
+console.log(calculateYear + (calculateYear - 42));
+
+let expression = "Estou mexendo no console.log";
+console.log(expression);
+
+let quartaFeira = true;
+console.log(quartaFeira);
+
+console.log(typeof quartaFeira);
+
